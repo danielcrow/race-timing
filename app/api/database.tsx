9bd5ec2faa508c7a3727eb51106b40c171d929d7
daniel.j.crow@gmail.com <daniel.j.crow@gmail.com>
2024-07-12 21:40:48 +0100

    here we go

--- app/api/database.tsx
+++ app/api/database.tsx
@@ -8,7 +8,7 @@ const options = {
 }
 
 let client;
-let clientPromise;
+let clientPromise: Promise<MongoClient>;
 
 if (!uri) {
     throw new Error('Please set Mongo URI')
@@ -16,13 +16,13 @@ if (!uri) {
 
 if (process.env['NODE_ENV'] === 'development') {
     if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options)
+        client = new MongoClient(uri)
         global._mongoClientPromise = client.connect()
     }
     clientPromise = global._mongoClientPromise
 } else {
 
-    client = new MongoClient(uri, options)
+    client = new MongoClient(uri)
     clientPromise = client.connect()
 }
 
--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,13 @@
 export {}
 
+import type { MongoClient } from 'mongodb'
+
+declare global {
+  namespace globalThis {
+    var _mongoClientPromise: Promise<MongoClient>
+  }
+}
+
 declare global {
     interface race {
       RaceId: string
