f78c57262d0f5fdb52d9e00d9149bda591937f31
daniel.j.crow@gmail.com <daniel.j.crow@gmail.com>
2024-07-12 21:55:22 +0100

    adding options

--- app/api/database.tsx
+++ app/api/database.tsx
@@ -1,11 +1,11 @@
 
-import { MongoClient } from 'mongodb';
+import { MongoClient,MongoClientOptions } from 'mongodb';
 
 const uri = process.env["MONGODB_URI"];
 const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-}
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+} as MongoClientOptions;
 
 let client;
 let clientPromise: Promise<MongoClient>;
@@ -16,13 +16,13 @@ if (!uri) {
 
 if (process.env['NODE_ENV'] === 'development') {
     if (!global._mongoClientPromise) {
-        client = new MongoClient(uri)
+        client = new MongoClient(uri,options)
         global._mongoClientPromise = client.connect()
     }
     clientPromise = global._mongoClientPromise
 } else {
 
-    client = new MongoClient(uri)
+    client = new MongoClient(uri,options)
     clientPromise = client.connect()
 }
 
